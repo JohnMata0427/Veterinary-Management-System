a6a1a6d05a24a852c5431451e642513a64ec9765
JohnMata0427 <john.mata@epn.edu.ec>
2024-08-06 19:23:47 -0500

    Project Compete

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,101 @@
+import axios from "axios";
+
+import { createContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+	const [auth, setAuth] = useState({});
+
+	const perfil = async (token) => {
+		try {
+			const response = await axios.get(
+				`${import.meta.env.VITE_BACKEND_URL}/perfil`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
+
+			setAuth(response.data);
+		} catch (error) {
+			try {
+				const response = await axios.get(
+					`${import.meta.env.VITE_BACKEND_URL}/paciente/perfil`,
+					{
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: `Bearer ${token}`,
+						},
+					}
+				);
+
+				setAuth(response.data);
+			} catch (error) {
+				localStorage.removeItem("token");
+				setAuth({});
+			}
+		}
+	};
+
+	const actualizarPerfil = async (datos) => {
+		const token = localStorage.getItem("token");
+		try {
+			const respuesta = await axios.put(
+				`${import.meta.env.VITE_BACKEND_URL}/veterinario/${datos.id}`,
+				datos,
+				{
+					headers: {
+						method: "PUT",
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			);
+			perfil(token);
+			return { respuesta: respuesta.data.res, exito: true };
+		} catch (error) {
+			return { respuesta: error.response.data.res, exito: false };
+		}
+	};
+
+	const actualizarPassword = async (datos) => {
+		try {
+			const respuesta = await axios.put(
+				`${
+					import.meta.env.VITE_BACKEND_URL
+				}/veterinario/actualizarpassword`,
+				datos,
+				{
+					headers: {
+						method: "PUT",
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${localStorage.getItem(
+							"token"
+						)}`,
+					},
+				}
+			);
+			return { respuesta: respuesta.data.res, exito: true };
+		} catch (error) {
+			return { respuesta: error.response.data.res, exito: false };
+		}
+	};
+
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		if (token) perfil(token);
+	}, []);
+
+	return (
+		<AuthContext.Provider
+			value={{ auth, setAuth, actualizarPerfil, actualizarPassword }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export { AuthContext };
